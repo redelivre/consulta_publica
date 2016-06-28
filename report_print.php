7048218f3ac5ad1e6f69681f9aa567ff3972acfc
Jacson Passold <jacson@ethymos.com.br>
2016-06-28 13:15:07 -0300

    walk on the loop

--- report_print.php
+++ report_print.php
@@ -2,7 +2,6 @@
 
 if (have_posts())
 {
-	the_post();
 	$output = fopen('php://output', 'w');
 	
 	$header = array(
@@ -38,37 +37,42 @@ if (have_posts())
 		header('Content-Disposition: attachment; filename='.date('Ymd').'_consulta_publica_report.csv');
 		
 		fputcsv($output, $header, ';');
-		$users_ids = get_post_meta(get_the_ID(), '_users_voto', true);
 		
-		foreach ($users_ids as $time => $user_id)
+		while (have_posts())
 		{
-			$index = 0;
-			$row = array();
-			$row[] = get_user_meta( $user_id, '_user_nome', true);
-			$row[] = get_user_meta( $user_id, '_user_cpf', true);
-			$row[] = get_user_meta( $user_id, '_user_email', true);
-			$row[] = get_user_meta( $user_id, '_user_telefone', true);
-			$row[] = get_user_meta( $user_id, '_user_municipio', true);
-			$row[] = get_user_meta( $user_id, '_user_uf', true);
-			$row[] = get_user_meta( $user_id, '_user_representatividade', true);
-			$row[] = get_user_meta( $user_id, '_user_instituicao', true);
-			$row[] = get_user_meta( $user_id, '_user_setorial_area', true);
-			$row[] = get_user_meta( $user_id, '_user_relatorio_radio', true);
-			$row[] = get_user_meta( $user_id, '_user_relatorio', true);
-			$row[] = get_user_meta( $user_id, '_user_primeira', true);
-			$row[] = get_user_meta( $user_id, '_user_segunda', true);
-			$row[] = get_user_meta( $user_id, '_user_terceira', true);
-			$row[] = get_user_meta( $user_id, '_user_quarta', true);
-			$row[] = get_user_meta( $user_id, '_user_quinta', true);
-			$row[] = get_user_meta( $user_id, '_user_sexta', true);
-			$row[] = get_user_meta( $user_id, '_user_desafio1', true);
-			$row[] = get_user_meta( $user_id, '_user_desafio2', true);
-			$row[] = get_user_meta( $user_id, '_user_solucao1', true);
-			$row[] = get_user_meta( $user_id, '_user_solucao2', true);
-			$row[] = get_user_meta( $user_id, '_user_atividade1', true);
-			$row[] = get_user_meta( $user_id, '_user_atividade2', true);
-			$row[] = get_user_meta( $user_id, '_user_documentosapoio', true);
-			fputcsv($output , $row, ';');
+			the_post();
+			$users_ids = get_post_meta(get_the_ID(), '_users_voto', true);
+			
+			foreach ($users_ids as $time => $user_id)
+			{
+				$index = 0;
+				$row = array();
+				$row[] = get_user_meta( $user_id, '_user_nome', true);
+				$row[] = get_user_meta( $user_id, '_user_cpf', true);
+				$row[] = get_user_meta( $user_id, '_user_email', true);
+				$row[] = get_user_meta( $user_id, '_user_telefone', true);
+				$row[] = get_user_meta( $user_id, '_user_municipio', true);
+				$row[] = get_user_meta( $user_id, '_user_uf', true);
+				$row[] = get_user_meta( $user_id, '_user_representatividade', true);
+				$row[] = get_user_meta( $user_id, '_user_instituicao', true);
+				$row[] = get_user_meta( $user_id, '_user_setorial_area', true);
+				$row[] = get_user_meta( $user_id, '_user_relatorio_radio', true);
+				$row[] = get_user_meta( $user_id, '_user_relatorio', true);
+				$row[] = get_user_meta( $user_id, '_user_primeira', true);
+				$row[] = get_user_meta( $user_id, '_user_segunda', true);
+				$row[] = get_user_meta( $user_id, '_user_terceira', true);
+				$row[] = get_user_meta( $user_id, '_user_quarta', true);
+				$row[] = get_user_meta( $user_id, '_user_quinta', true);
+				$row[] = get_user_meta( $user_id, '_user_sexta', true);
+				$row[] = get_user_meta( $user_id, '_user_desafio1', true);
+				$row[] = get_user_meta( $user_id, '_user_desafio2', true);
+				$row[] = get_user_meta( $user_id, '_user_solucao1', true);
+				$row[] = get_user_meta( $user_id, '_user_solucao2', true);
+				$row[] = get_user_meta( $user_id, '_user_atividade1', true);
+				$row[] = get_user_meta( $user_id, '_user_atividade2', true);
+				$row[] = get_user_meta( $user_id, '_user_documentosapoio', true);
+				fputcsv($output , $row, ';');
+			}
 		}
 	}
 	elseif(intval(get_query_var('consultas_print_xls', 0)))
@@ -93,37 +97,40 @@ if (have_posts())
 		fputs($output, utf8_decode("
 			    </tr>"
 		));
-		
-		$users_ids = get_post_meta(get_the_ID(), '_users_voto', true);
-		
-		foreach ($users_ids as $time => $user_id)
+		while (have_posts())
 		{
-		   fputs($output, '<tr>');
-				fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_nome', true)).'</td>');
-				fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_cpf', true)).'</td>');
-				fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_email', true)).'</td>');
-				fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_telefone', true)).'</td>');
-				fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_municipio', true)).'</td>');
-				fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_uf', true)).'</td>');
-				fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_representatividade', true)).'</td>');
-				fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_instituicao', true)).'</td>');
-				fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_setorial_area', true)).'</td>');
-				fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_relatorio_radio', true)).'</td>');
-				fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_relatorio', true)).'</td>');
-				fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_primeira', true)).'</td>');
-				fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_segunda', true)).'</td>');
-				fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_terceira', true)).'</td>');
-				fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_quarta', true)).'</td>');
-				fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_quinta', true)).'</td>');
-				fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_sexta', true)).'</td>');
-				fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_desafio1', true)).'</td>');
-				fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_desafio2', true)).'</td>');
-				fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_solucao1', true)).'</td>');
-				fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_solucao2', true)).'</td>');
-				fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_atividade1', true)).'</td>');
-				fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_atividade2', true)).'</td>');
-				fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_documentosapoio', true)).'</td>');
-		   fputs($output, '</tr>');
+			the_post();
+			$users_ids = get_post_meta(get_the_ID(), '_users_voto', true);
+			
+			foreach ($users_ids as $time => $user_id)
+			{
+			   fputs($output, '<tr>');
+					fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_nome', true)).'</td>');
+					fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_cpf', true)).'</td>');
+					fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_email', true)).'</td>');
+					fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_telefone', true)).'</td>');
+					fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_municipio', true)).'</td>');
+					fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_uf', true)).'</td>');
+					fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_representatividade', true)).'</td>');
+					fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_instituicao', true)).'</td>');
+					fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_setorial_area', true)).'</td>');
+					fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_relatorio_radio', true)).'</td>');
+					fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_relatorio', true)).'</td>');
+					fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_primeira', true)).'</td>');
+					fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_segunda', true)).'</td>');
+					fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_terceira', true)).'</td>');
+					fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_quarta', true)).'</td>');
+					fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_quinta', true)).'</td>');
+					fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_sexta', true)).'</td>');
+					fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_desafio1', true)).'</td>');
+					fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_desafio2', true)).'</td>');
+					fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_solucao1', true)).'</td>');
+					fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_solucao2', true)).'</td>');
+					fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_atividade1', true)).'</td>');
+					fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_atividade2', true)).'</td>');
+					fputs($output, '<td>'.utf8_decode(get_user_meta( $user_id, '_user_documentosapoio', true)).'</td>');
+			   fputs($output, '</tr>');
+			}
 		}
 		fputs($output, '</table>');
 	}
